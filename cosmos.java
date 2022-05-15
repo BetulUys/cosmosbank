ec9d71c6869ab99505dfba4135c4c922ca60a0ae
fersatir <fatihersatir@gmail.com>
2022-05-15 18:52:06 +0300

    fatih v1

--- cosmos.java
+++ cosmos.java
@@ -1,7 +1,7 @@
 public class cosmos {
     public static void main(String[] args) {
         System.out.println("cosmos");
-
+        System.out.println("fatih");
         System.out.println("deneme samet");
     }
 }
